5da92e86264944d86f5517588961101467e10061
Ashwin <aswinsingh501@gmail.com>
2026-04-13 15:59:49 +0530

    st1

---
+++ main.jsx
@@ -0,0 +1,7 @@
+let heading = document.getElementById("mainheading");
+let paragraph = document.getElementById("paragraph");
+let input = document.getElementById("UserInput");
+
+let fontsize = 16;
+
+document.getElementById("")
